444cbb49f7176332e035b3c02b79ebc4911d2743
erickson-makotoki <a820224@gmail.com>
2014-12-15 00:12:36 +0800

    try using layout manager

--- src/net/ericksonjuang/DrawBoard.java
+++ src/net/ericksonjuang/DrawBoard.java
@@ -1,18 +1,63 @@
 package net.ericksonjuang;
 
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
+import java.awt.*;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class DrawBoard extends JPanel {
     private int[][] board;
     private static final Color bg = new Color(233, 186, 30);
-
+    private static JButton start, restart, undo;
     public DrawBoard(int[][] board)
     {
         this.board = board;
+        //Prepare Button
+        JPanel buttonContainer = new JPanel(new GridBagLayout());
+        start = new JButton("Start");
+        restart = new JButton("Restart");
+        undo = new JButton("Undo");
+        buttonContainer.add(start, new GridBagConstraints() {
+            {
+                this.gridx = 3;
+                this.gridy = 0;
+                this.gridwidth = 2;
+                this.gridheight = 1;
+                this.weightx = 0;
+                this.weighty = 0;
+                this.fill = GridBagConstraints.HORIZONTAL;
+                this.anchor = GridBagConstraints.CENTER;
+            }
+
+        });
+        buttonContainer.add(restart, new GridBagConstraints() {
+            {
+                this.gridx = 3;
+                this.gridy = 2;
+                this.gridwidth = 2;
+                this.gridheight = 1;
+                this.weightx = 0;
+                this.weighty = 0;
+                this.fill = GridBagConstraints.HORIZONTAL;
+                this.anchor = GridBagConstraints.CENTER;
+            }
+
+        });
+        buttonContainer.add(undo, new GridBagConstraints() {
+            {
+                this.gridx = 3;
+                this.gridy = 4;
+                this.gridwidth = 2;
+                this.gridheight = 1;
+                this.weightx = 0;
+                this.weighty = 0;
+                this.fill = GridBagConstraints.HORIZONTAL;
+                this.anchor = GridBagConstraints.CENTER;
+            }
+
+        });
+        buttonContainer.setSize(200, 300);
+        add(buttonContainer);
+        setSize(800, 640);
 
     }
 
@@ -64,5 +109,6 @@ public class DrawBoard extends JPanel {
         //white point
         g.setColor(Color.WHITE);
         g.fillArc(590, 170, 50, 50, 0, 360);
+
     }
 }
--- src/net/ericksonjuang/Main.java
+++ src/net/ericksonjuang/Main.java
@@ -10,14 +10,14 @@ public class Main
     static final Board game = new Board();
     static final JFrame window = new JFrame("黑白棋 by Erickson Juang");
     static int player;
-    static JButton start, restart, undo;
+    //static JButton start, restart, undo;
     public static void main ( String[] arg)
     {
         //init
         player = State.Black;
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(800, 640);
-        window.setLayout(new GridBagLayout());
+        window.setResizable(false);
 
 
         //prepare menu bar
@@ -39,66 +39,12 @@ public class Main
             }
         });
 
-        //Prepare Button
-        start = new JButton("Start");
-        restart = new JButton("Restart");
-        undo = new JButton("Undo");
-        window.add(start, new GridBagConstraints() {
-            {
-                this.gridx = 1;
-                this.gridy = 0;
-                this.gridwidth = 2;
-                this.gridheight = 1;
-                this.weightx = 0;
-                this.weighty = 0;
-                this.fill = GridBagConstraints.HORIZONTAL;
-                this.anchor = GridBagConstraints.CENTER;
-            }
-
-        });
-        window.add(restart, new GridBagConstraints() {
-            {
-                this.gridx = 1;
-                this.gridy = 2;
-                this.gridwidth = 2;
-                this.gridheight = 1;
-                this.weightx = 0;
-                this.weighty = 0;
-                this.fill = GridBagConstraints.HORIZONTAL;
-                this.anchor = GridBagConstraints.CENTER;
-            }
-
-        });
-        window.add(undo, new GridBagConstraints() {
-            {
-                this.gridx = 1;
-                this.gridy = 4;
-                this.gridwidth = 2;
-                this.gridheight = 1;
-                this.weightx = 0;
-                this.weighty = 0;
-                this.fill = GridBagConstraints.HORIZONTAL;
-                this.anchor = GridBagConstraints.CENTER;
-            }
-
-        });
-
 
         //畫棋盤
         DrawBoard drawing = new DrawBoard(game.getBoard());
 
 
-        window.add(drawing, new GridBagConstraints() {
-            {
-                this.gridx = 0;
-                this.gridy = 0;
-                this.gridwidth = 4;
-                this.gridheight = 4;
-                this.fill = GridBagConstraints.BOTH;
-                this.anchor = GridBagConstraints.CENTER;
-            }
-
-        });
+        window.add(drawing);
 
 
         window.setVisible(true);
